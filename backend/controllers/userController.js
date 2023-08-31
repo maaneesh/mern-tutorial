03e24b211a7a41d6615a4de87dd1ec124baefcca
Manish Chaudhari <chaudharimanish07@gmail.com>
2023-08-31 10:21:19 -0400

    added login and getMe routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,37 @@
 
-
+// @desc Register new USer
+// @desc POST /api/users
+//@access Public
 
 const registerUser = (req, res)=>{
     res.json({message: 'Register User'})
 
 }
 
+// @desc Register new USer
+// @desc POST /api/users/login
+//@access Public
+
+const loginUser = (req, res)=>{
+    res.json({message: 'Login User'})
+
+}
+
+// @desc Get User data
+// @desc GET /api/users/me
+//@access Public
+
+const getMe = (req, res)=>{
+    res.json({message: 'User Data disply'})
+
+}
+
+
+
+
+
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser,
+    getMe
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,16 @@
 const express = require("express");
-const router = express.Router()
+const router = express.Router();
 const {
-    registerUser,
+  registerUser,
+  userRoutes,
+  getMe,
 } = require("../controllers/userController");
-
+const { loginUser } = require("../controllers/userController");
 
 //Create a user
-router.post('/',registerUser)
+router.post("/", registerUser);
 
+router.post("/login", loginUser);
+router.get("/me", getMe);
 
 module.exports = router;
