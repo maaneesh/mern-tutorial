14f2a398707e5a5c9a3a264d8ae844461988b75d
Manish Chaudhari <chaudharimanish07@gmail.com>
2023-09-05 23:09:57 -0400

    setup for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
-const connectDB = require('./config/db')
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5040;
-
 
 connectDB();
 
@@ -15,7 +15,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
-app.use(errorHandler)
+//Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send('Please set to production'));
+}
+
+app.use(errorHandler);
 
 //Server
 
